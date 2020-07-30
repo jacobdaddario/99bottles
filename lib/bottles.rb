74ddc5b790d5c0ab94bf28edd4530f5d3f786a5c
Jacob Daddario <jake.daddario@gmail.com>
2020-07-29 21:46:03 -0500

    Shameless green

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,51 +1,34 @@
 class Bottles
-  def verse(bottles)
-    @bottles = bottles
-    define_script
-
-    first_verse = "#{@bottles.to_s.capitalize} #{@noun} of beer on the wall, #{@bottles} #{@noun} of beer."
-
-    unless @bottles.is_a?(String)
-      decrement_bottles
-      update_script
-
-      second_verse = "Take #{@object} down and pass it around, #{@bottles} #{@noun} of beer on the wall."
-    else
-      @bottles = 99
-      second_verse = "Go to the store and buy some more, #{@bottles} #{@noun} of beer on the wall."
-    end
-
-    "#{first_verse}\n#{second_verse}\n"
-  end
-
-  def verses(first, last)
-    song = ""
-
-    first.downto(last) do |bottles|
-      song << "#{verse(bottles)}\n"
-    end
-
-    song.chop
-  end
-
   def song
     verses(99, 0)
   end
 
-    private
+  def verses(starting, ending)
+    starting.downto(ending).map { |i| verse(i) }.join("\n")
+  end
 
-    def decrement_bottles
-      @bottles -= 1
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n" 
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number - 1} bottles of beer on the wall.\n"
     end
-
-    def define_script
-      @noun = @bottles != 1 ? "bottles" : "bottle"
-      @object = @bottles != 1 ? "one" : "it"
-      @bottles = @bottles == 0 ? "no more" : @bottles
-    end
-
-    def update_script
-      @noun = @bottles != 1 ? "bottles" : "bottle"
-      @bottles = @bottles == 0 ? "no more" : @bottles
-    end
+  end
 end
